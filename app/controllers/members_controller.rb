927f2dd78e4946bd6d45dc573e5ee4db0aadd9b8
伊藤脩一郎 <ahuro110.play@gmail.com>
2020-05-08 22:20:55 +0900

    finish_crud_method

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -9,12 +9,32 @@ class MembersController < ApplicationController
     @member = Member.new(birthday: Date.new(1980,1,1))
   end
   def create
+    @member = Member.new(params[:member])
+    if @member.save
+      flash[:notice] = "会員を登録しました"
+      redirect_to member_path(@member)
+    else
+      render "new"
+    end
   end
   def edit
+    @member = Member.find(params[:id])
   end
   def update
+    @member = Member.find(params[:id])
+    @member.update(params[:member])
+    if @member.save
+      flash[:notice] = "会員情報を更新しました"
+      redirect_to member_path(@member)
+    else
+      render "edit"
+    end
   end
   def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    flash[:notice] = "会員情報を削除しました"
+    redirect_to members_path
   end
   def search
     @members = Member.search(params[:q])
